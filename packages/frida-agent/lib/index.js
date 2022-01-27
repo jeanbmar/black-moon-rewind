77bb1d1194dfac584fa4c05852e861481008146d
Jean-Baptiste <ultrapowa.exp@gmail.com>
2022-01-27 11:34:38 +0100

    add packet logging and disable crypto

--- packages/frida-agent/lib/index.js
+++ packages/frida-agent/lib/index.js
@@ -1,5 +1,25 @@
+const disableCrypto = () => {
+    Interceptor.attach(ptr(0x50D640), {
+        onLeave(value) {
+            value.replace(0);
+        },
+    });
+};
+
+const hookPackets = (cb) => {
+    Interceptor.attach(ptr(0x492e60), function() {
+        const to = this.context.sp.add(4).readPointer();
+        const len = to.add(20).readInt();
+        const buf = to.add(16).readPointer();
+        cb(buf.readByteArray(len), 1);
+    });
+};
+
+
 const init = async () => {
-    console.log(`debugging session started at ${new Date()}`);
+    console.log('process', Process.id, Process.arch, Process.platform);
+    disableCrypto();
+    hookPackets((buf) => console.log(buf));
 };
 
 rpc.exports = { init };
