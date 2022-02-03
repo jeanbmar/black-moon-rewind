60322da45aed1fb89aff3bf7ab929a4529c3dd62
Jean-Baptiste <ultrapowa.exp@gmail.com>
2022-02-03 12:38:30 +0100

    scale light and quickslot

--- packages/frida-agent/lib/display.js
+++ packages/frida-agent/lib/display.js
@@ -45,6 +45,21 @@ const upscaleBoxUI = (factor) => {
     // upscaleU16(ptr(0x4520B4).add(1), factor); // do not scale as we stick to top
 };
 
+const upscaleLight = (factor) => {
+    // commented scaling apply to 1024 and 2048 values, they may be unrelated
+    // upscaleU16(ptr(0x45f97b).add(2), factor);
+    // upscaleU16(ptr(0x45F9BF).add(2), factor);
+    // upscaleU16(ptr(0x45F9C7).add(2), factor);
+    // upscaleU16(ptr(0x45fa21).add(2), factor);
+    upscaleU16(ptr(0x45fa2c).add(1), factor);
+    upscaleU16(ptr(0x45fa38).add(1), factor);
+    upscaleU16(ptr(0x45FA3F).add(3), factor);
+    upscaleU16(ptr(0x49a03c).add(1), factor);
+    upscaleU16(ptr(0x49A046).add(2), factor);
+    // upscaleU16(ptr(0x49A04e).add(1), factor);
+    // upscaleU16(ptr(0x49A057).add(2), factor);
+};
+
 const upscaleTaskBarUI = (factor) => {
     upscaleU16(ptr(0x5136aA).add(1), (value) => HEIGHT * factor - (HEIGHT - value));
     upscaleU16(ptr(0x5136B2).add(1), factor);
@@ -73,6 +88,11 @@ const upscaleMapUI = (factor) => {
     upscaleU16(ptr(0x437c86).add(1), () => ptr(0x437c76).add(1).readU16() + (581 - 411)); // scale for map button
 };
 
+const upscaleQuickslotUI = (factor) => {
+    // upscaleU16(ptr(0x40C424).add(6), factor); // no scaling, keep it mid screen
+    upscaleU16(ptr(0x40C42e).add(6), () => HEIGHT * factor * 0.8);
+};
+
 const upscaleSplashScreen = (factor) => {
     Interceptor.attach(ptr(0x40B04E), function() {
         this.context.esi.writeInt(WIDTH * (factor - 1) / 2);
@@ -91,12 +111,14 @@ const upscaleSplashScreen = (factor) => {
 const upscale = (factor = 1.8) => {
     upscaleWindow(factor);
     upscaleBoxUI(factor);
+    upscaleLight(factor);
     upscaleUIScaling(factor);
     upscaleStatusUI(factor);
     upscaleChatUI(factor);
     upscaleMapUI(factor);
     upscaleTaskBarUI(factor);
     upscaleSplashScreen(factor);
+    upscaleQuickslotUI(factor);
 };
 
 module.exports = {
