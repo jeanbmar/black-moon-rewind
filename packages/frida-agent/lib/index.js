dfebf06f81c5ea1c324520cd88e56a3814698f9e
Jean-Baptiste <ultrapowa.exp@gmail.com>
2022-02-10 11:18:43 +0100

    remove debugging data

--- packages/frida-agent/lib/index.js
+++ packages/frida-agent/lib/index.js
@@ -2,13 +2,16 @@ const network = require('./network');
 const debug = require('./debug');
 const display = require('./display');
 
-const init = async () => {
+const init = async (stage, options = {}) => {
+    const { env = {} } = options;
     console.log('process', Process.id, Process.arch, Process.platform);
     network.disableCrypto();
     network.enableTCP();
     network.disableSynAck();
-    debug.hookDebugLog((message) => send({ type: 'stdout', message }));
-    debug.hookPackets((buf, direction) => console.log(direction === 1 ? 'S' : 'R', buf));
+    if (env.NODE_ENV === 'development') {
+        debug.hookDebugLog((message) => send({ type: 'stdout', message }));
+        debug.hookPackets((buf, direction) => console.log(direction === 1 ? 'S' : 'R', buf));
+    }
     display.upscale();
 };
 
